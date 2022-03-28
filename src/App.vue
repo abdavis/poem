36aba05b0d42c8c3a30b772ba544af15159c7b79
Andrew Davis <abdavis7@gmail.com>
2022-03-28 11:50:32 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import { ref } from 'vue';
+let lines = ref([
+  "Experience suggests beggining, middle, end. But maybe...",
+  "What if first became last, and last was first?",
+  "Can everything happen all at once?",
+  "Can you imagine a world where order is void?",
+  "Nothing makes sense without sequence, right?"
+]);
+
+function shuffle(a: any[]) {
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+}
+
+shuffle(lines.value);
+</script>
+
+<template>
+  <TransitionGroup name="poem">
+    <p v-for="line in lines" :key="line">{{ line }}</p>
+  </TransitionGroup>
+  <button @click="shuffle(lines)">Shuffle</button>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.poem-move {
+  transition: all 1.5s ease;
+}
+</style>
