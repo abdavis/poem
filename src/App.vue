9054614a6187f496eb47d52797bee5f94770e601
Andrew Davis <abdavis7@gmail.com>
2022-04-02 12:46:27 -0600

    Added wasm compression module

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,34 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import { ref } from 'vue';
-let lines = ref([
+import init, { compress, decompress } from './assets/brotli_wasm/wasm_compress';
+let lines = ref();
+const default_lines = [
   "Experience suggests beggining, middle, end. But maybe...",
   "What if first became last, and last was first?",
   "Can everything happen all at once?",
   "Can you imagine a world where order is void?",
   "Nothing makes sense without sequence, right?"
-]);
+];
+
+const test_lines = [
+  "lorem ispum sit amet",
+  "here is a string of random words",
+  "and again, some nonsense for you to read",
+  "the quick brown fox jumped over the lazy dog"
+]
+
+//load wasm file first, then we can decompress hash
+init().then(() => {
+  try {
+    lines.value = JSON.parse(decompress(location.hash.slice(1)));
+  } catch {
+    lines.value = default_lines;
+  }
+  shuffle(lines.value);
+  shuffle(lines.value);
+});
+
 
 function shuffle(a: any[]) {
   for (let i = a.length - 1; i > 0; i--) {
@@ -16,8 +37,6 @@ function shuffle(a: any[]) {
     [a[i], a[j]] = [a[j], a[i]];
   }
 }
-
-shuffle(lines.value);
 </script>
 
 <template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import gzipPlugin from 'rollup-plugin-gzip'
+import { brotliCompressSync, constants } from 'zlib'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue(), gzipPlugin({ filter: /\.(js|mjs|json|css|html|wasm)$/ }), gzipPlugin({
+    customCompression: content => brotliCompressSync(Buffer.from(content), {
+      [constants.BROTLI_PARAM_QUALITY]: 11
+    }),
+    fileName: '.br',
+    filter: /\.(js|mjs|json|css|html|wasm)$/
+  })]
 })
